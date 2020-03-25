b42dc2b6e84501fe7749f9fc14ae515085d68ed6
GR <grbusiness18@gmail.com>
2020-03-25 11:06:49 +0100

    fix

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ angular.module('app', [])
 
     var gridApp = this;
     
+    $scope.rw = []
+    $scope.cl = []
+    $scope.diag = []
     $scope.movecounter = 0; 
     $scope.shwGenerate = true;
     $scope.shwReset = false;
     $scope.showGameOver = false;
-
     $scope.gridclass = [[]];
+    $scope.temp = [1,2,3,4,5]
 
     $scope.chunk = function(a){
         results = []
@@ -27,16 +30,20 @@ angular.module('app', [])
         var arr = [...Array(25).keys()].map(x => x +1 );
         arr.sort(() => Math.random() - 0.5);        
         $scope.aGrid = $scope.chunk(arr);  
+        this.reset();
     }
 
     gridApp.reset = function(){
+        $scope.rw = []
+        $scope.cl = []
+        $scope.diag = []
         $scope.movecounter = 0; 
         $scope.shwGenerate = true;
         $scope.shwReset = false;
         $scope.showGameOver = false;
         $scope.aGrid = $scope.chunk([...Array(25).keys()].map(x => x +1 ))
         $scope.aGridEnb = $scope.chunk([...Array(25).keys()].map(x => false));
-        $scope.bingo = [...Array(25).keys()].map(x => false )
+        $scope.bingo = [...Array(5).keys()].map(x => false )        
     }
 
 
@@ -45,6 +52,9 @@ angular.module('app', [])
     }
 
     gridApp.gridClick = function(idx, sidx){
+        if ($scope.showGameOver){
+            return 
+        }
         if ($scope.movecounter >= 25 ){
             // game over
             $scope.showGameOver = true
@@ -54,11 +64,65 @@ angular.module('app', [])
             if ($scope.aGridEnb[idx][sidx] === false){
                 $scope.aGridEnb[idx][sidx] = true;
                 $scope.movecounter =  $scope.movecounter + 1;
-            }            
+            }  
+            
+            $scope.crossLine()
+            $scope.gameover()
         }        
     }
 
     $scope.crossLine = function(){
-        
+        $scope.rw = []
+        $scope.cl = []
+        $scope.diag = []
+       
+        $scope.aGridEnb.forEach((el , idx) => {
+            if (el.every(v=> v === true)){
+                $scope.rw.push(idx)
+            }
+
+            var cl_vals = []
+            $scope.temp.forEach((cel, cidx) =>{
+                cl_vals.push($scope.aGridEnb[cidx][idx])
+            });
+
+            if (cl_vals.length == 5 && cl_vals.every(v=> v === true)){
+                $scope.cl.push(idx)
+            }
+        });
+
+        // diagonal
+        var diag_x = [$scope.aGridEnb[0][0],$scope.aGridEnb[1][1],
+        $scope.aGridEnb[2][2], $scope.aGridEnb[3][3], $scope.aGridEnb[4][4]]
+
+        if (diag_x.every(v=> v=== true)){
+            $scope.diag.push("x")
+        }
+
+        // diagonal
+        var diag_y = [$scope.aGridEnb[0][4],$scope.aGridEnb[1][3],
+        $scope.aGridEnb[2][2], $scope.aGridEnb[3][1], $scope.aGridEnb[4][0]]
+
+        if (diag_y.every(v=> v=== true)){
+            $scope.diag.push("y")
+        }
     };
+
+
+    $scope.gameover = function(){
+        var game = ($scope.rw.length + $scope.cl.length + $scope.diag.length) || 0;
+        console.log("Game", game);
+        if (game > 4) {
+            $scope.showGameOver = true
+        }
+
+
+        if (game > 0){
+            for (let i = 1; i <= game; i++) {
+                $scope.bingo[i-1] = true
+              }            
+        }            
+        
+    }
+
   });
